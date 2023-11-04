ddb3665bdf670bf105e25d8828480d26a131de16
shenqing <742697164@qq.com>
2023-11-04 14:32:22 +0800

    eslint配置

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,19 @@
 import { defineStore } from "pinia";
 
-export const useDemoStore = defineStore('demo', ()=>{
+export const useDemoStore = defineStore('demo', () => {
     // state
     let count = ref(1)
     // action, 异步的同样写法
     function addCount() {
-        count++    
+        count++
     }
     // getter
-    let count10 = computed(()=>{
-        return count * 10    
+    let count10 = computed(() => {
+        return count * 10
     })
     return {
         count,
         addCount,
-        count10    
+        count10
     }
 })
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),
@@ -11,7 +13,11 @@ export default defineConfig({
 		'vue-router',
 		'pinia',
 	  ],
+	  resolvers: [ElementPlusResolver()],
 	   // 指明 .d.ts 文件的位置和文件名，生成 `auto-import.d.ts` 全局声明
 	  dts: 'src/types/auto-import.d.ts',
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
     }),],
 })
