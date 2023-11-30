354052ca4d430d7e77926a35553a4399b60f4ff9
shenqing <742697164@qq.com>
2023-11-30 13:16:36 +0800

    update

--- src/main.js
+++ src/main.js
@@ -7,5 +7,7 @@ const pinia = createPinia()
 import * as echarts from 'echarts'
 import vChart from 'vue-echarts'
 const app = createApp(App)
-app.use(pinia).use(router).mount('#app').component('v-chart', vChart)
-app.config.globalProperties.$echarts = echarts
+app.component('v-chart', vChart)
+app.use(pinia).use(router)
+app.mount('#app').
+    app.config.globalProperties.$echarts = echarts
--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,19 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-    AutoImport({
-	  imports: [
-		// 需要自动导入的API，自动导入vue和vue-router相关函数
-		'vue',
-		'vue-router',
-		'pinia',
-	  ],
-	  resolvers: [ElementPlusResolver()],
-	   // 指明 .d.ts 文件的位置和文件名，生成 `auto-import.d.ts` 全局声明
-	  dts: 'src/types/auto-import.d.ts',
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),],
+	plugins: [vue(),
+	AutoImport({
+		imports: [
+			// 需要自动导入的API，自动导入vue和vue-router相关函数
+			'vue',
+			'vue-router',
+			'pinia',
+		],
+		resolvers: [ElementPlusResolver()],
+		// 指明 .d.ts 文件的位置和文件名，生成 `auto-import.d.ts` 全局声明
+		dts: 'src/types/auto-import.d.ts',
+	}),
+	Components({
+		resolvers: [ElementPlusResolver()],
+	}),],
 })
